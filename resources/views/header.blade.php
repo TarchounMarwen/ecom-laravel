3b420b2a4b896ed8cdda1c2a5b5d722239342df7
unknown <marwentarchoun.develoeper@gmail.com>
2021-03-17 21:29:48 +0100

    header , footer and login page

---
+++ resources/views/header.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Brand</a>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Orders</a>
+        </li>
+
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+          <li> card(1)</li>
+      </ul>
+    </div>
+  </nav>
